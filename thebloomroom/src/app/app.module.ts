f3c1f4d6c554c1d670dcea60a9302496c150d3db
gowthamiedamalapati <gouthami1812@gmail.com>
2021-01-26 22:40:13 +0530

    added components Signin header footer landing page

--- thebloomroom/src/app/app.module.ts
+++ thebloomroom/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent,
+    SignInComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
