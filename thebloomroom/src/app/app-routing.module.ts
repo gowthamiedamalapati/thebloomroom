e356d23f7c0e2ed2b22432663db98856b15a3eec
gowthamiedamalapati <gouthami1812@gmail.com>
2021-02-13 09:38:01 +0530

    Css changes

--- thebloomroom/src/app/app-routing.module.ts
+++ thebloomroom/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { ProductsComponent } from './products/products.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CartComponent } from './cart/cart.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
 const routes: Routes = [
   { path:'', redirectTo:'/shop', pathMatch: 'full'},
+  { path: 'Land', component: LandingpageComponent},
   { path: 'signin', component: SignInComponent },
   { path: 'register', component: RegistrationComponent },
   { path: 'shop', component: ProductsComponent},
+  { path: 'cart', component: CartComponent},
   { path: '**', component: PageNotFoundComponent }
 
 ];
